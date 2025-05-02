f760819117224670fe695036c74d65a6b08a931e
Petite Graine <remi93300@hotmail.fr>
2025-05-02 10:22:49 +0200

    feat: UI for each cube

--- Assets/Scripts/DraggableObject.cs
+++ Assets/Scripts/DraggableObject.cs
@@ -1,50 +1,69 @@
+using System;
+using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class DraggableObject : MonoBehaviour
 {
-    private Camera mainCamera;
-    private bool isDragging = false;
-    private float fixedY = 0.5f;
-    private Plane dragPlane;
-    private Vector3 offset;
+    [Header("Drag Settings")]
+    private Camera _mainCamera;
+    private bool _isDragging = false;
+    private float _fixedY = 0.5f;
+    private Plane _dragPlane;
+    private Vector3 _offset;
+    private bool _isDraggable = true;
 
-    public bool isDraggable = true;
+    [Header("Timer")]
+    private bool _isTimerOn = false;
+    private TimeSpan _timePlaying;
+    private float _elapsedTime = 0f;
+    private int _clics = 0;
+    private int _errors = 0;
 
-    void Start()
+    [Header("UI Elements")]
+    [SerializeField] private TextMeshProUGUI _timerText;
+    [SerializeField] private TextMeshProUGUI _errorsText;
+    [SerializeField] private TextMeshProUGUI _clicsText;
+
+    private void Start()
     {
-        mainCamera = Camera.main;
+        _mainCamera = Camera.main;
+        _timerText.text = "Temps : 00:00.00";
     }
 
-    void OnMouseDown()
+    private void OnMouseDown()
     {
-        if (!isDraggable) return;
-        dragPlane = new Plane(Vector3.up, new Vector3(0, fixedY, 0));
+        if (!_isDraggable) return;
+        IncrementClics();
+        if (!_isTimerOn) { BeginTimer(); }
+        _dragPlane = new Plane(Vector3.up, new Vector3(0, _fixedY, 0));
 
-        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-        if (dragPlane.Raycast(ray, out float enter))
+        Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (_dragPlane.Raycast(ray, out float enter))
         {
             Vector3 hitPoint = ray.GetPoint(enter);
-            offset = transform.position - hitPoint;
-            isDragging = true;
+            _offset = transform.position - hitPoint;
+            _isDragging = true;
         }
     }
 
-    void OnMouseDrag()
+    private void OnMouseDrag()
     {
-        if (!isDraggable || !isDragging) return;
+        if (!_isDraggable || !_isDragging) return;
 
-        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-        if (dragPlane.Raycast(ray, out float enter))
+        Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (_dragPlane.Raycast(ray, out float enter))
         {
             Vector3 hitPoint = ray.GetPoint(enter);
-            Vector3 targetPos = hitPoint + offset;
-            transform.position = new Vector3(targetPos.x, fixedY, targetPos.z);
+            Vector3 targetPos = hitPoint + _offset;
+            transform.position = new Vector3(targetPos.x, _fixedY, targetPos.z);
         }
     }
 
-    void OnMouseUp()
+    private void OnMouseUp()
     {
-        isDragging = false;
+        if (!_isDraggable) return;
+        _isDragging = false;
 
         Collider[] hitZones = Physics.OverlapBox(transform.position, transform.localScale / 2f, Quaternion.identity);
 
@@ -53,12 +72,14 @@ public class DraggableObject : MonoBehaviour
             if (col.CompareTag("Zone") && IsFullyInsideXZ(col))
             {
                 SetPlacement(col.transform.position.x, col.transform.position.y + 0.6f, col.transform.position.z);
+                EndTimer();
                 break;
             }
         }
+        if (_isDraggable) IncrementErrors();
     }
 
-    bool IsFullyInsideXZ(Collider zone)
+    private bool IsFullyInsideXZ(Collider zone)
     {
         Bounds zoneBounds = zone.bounds;
         Bounds myBounds = GetComponent<Collider>().bounds;
@@ -81,9 +102,44 @@ public class DraggableObject : MonoBehaviour
         return true;
     }
 
-    public void SetPlacement(float x, float y, float z)
+    private void SetPlacement(float x, float y, float z)
     {
-        isDraggable = false;
+        _isDraggable = false;
         transform.position = new Vector3(x, y, z);
+    }
+    private void IncrementErrors()
+    {
+        _errors++;
+        _errorsText.text = "Erreurs : " + _errors;
+    }
+    private void IncrementClics()
+    {
+        _clics++;
+        _clicsText.text = "Clics :" + _clics;
+    }
+
+
+    private void BeginTimer()
+    {
+        _isTimerOn = true;
+        _elapsedTime = 0f;
+        StartCoroutine(UpdateTimer());
+    }
+
+    private void EndTimer()
+    {
+        _isTimerOn = false;
+    }
+    private IEnumerator UpdateTimer()
+    {
+        while (_isTimerOn)
+        {
+            _elapsedTime += Time.deltaTime;
+            _timePlaying = TimeSpan.FromSeconds(_elapsedTime);
+            string timePlayingStr = "Time: " + _timePlaying.ToString("mm':'ss'.'ff");
+            _timerText.text = timePlayingStr;
+
+            yield return null;
+        }
     }
 }
